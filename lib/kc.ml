34fc33964a013dd1e1c5ba11f571422858b466ab
Minsung <minsung@ccs.neu.edu>
2023-09-14 22:45:37 -0400

    update deps for linux build

--- lib/kc.ml
+++ lib/kc.ml
@@ -1,3 +1,17 @@
 (** inference via knowledge compilation *)
 
+open Rsdd
+
+let () =
+  let robdd_builder = mk_bdd_builder_default_order 6L in
+    let bdd = bdd_builder_compile_cnf robdd_builder (cnf_from_dimacs "
+    p cnf 6 3\n\
+    1 2 3 4 0\n\
+    -2 -3 4 5 0\n\
+    -4 -5 6 6 0\n\
+    ") in
+    let model_count = bdd_model_count robdd_builder bdd in
+    print_endline (Int64.to_string model_count)
+
+
 let infer _prog = failwith "todo"
